9a0793793d2075e32f21c4e5c891c36838a62c04
nguyenlong1019 <nguyenvanlong17052002@gmail.com>
2024-10-22 22:45:09 +0700

    Fix user route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,16 +1,8 @@
 import express from 'express';
 const router = express.Router();
-import {generateToken, registerUser, loginUser, changePassword, createUser, getAllUsers, getUserById, updateUser, deleteUser} from '../controllers/userController.js';
-import {verifyToken, isAdmin} from '../middlewares/authMiddleware.js';
+import {createUser, getAllUsers, getUserById, updateUser, deleteUser} from '../controllers/userController.js';
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.post('/change-password', changePassword);
-// router.post('/', [verifyToken, isAdmin], userController.createUser);
-// router.get('/', [verifyToken, isAdmin], userController.getAllUsers);
-// router.get('/:id', [verifyToken, isAdmin], userController.getUserById);
-// router.put('/:id', [verifyToken, isAdmin], userController.updateUser);
-// router.delete('/:id', [verifyToken, isAdmin], userController.deleteUser);
+
 
 router.post('/', createUser);
 router.get('/', getAllUsers);
