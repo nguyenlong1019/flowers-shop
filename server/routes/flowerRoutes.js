1e9ab88c295d7453971a412d35078fe08598d223
nguyenlong1019 <nguyenvanlong17052002@gmail.com>
2024-10-30 11:21:01 +0700

    update flower router

--- server/routes/flowerRoutes.js
+++ server/routes/flowerRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
 const router = express.Router();
-import {createFlower, getAllFlowers, getFlowerById, updateFlower, deleteFlower} from '../controllers/flowerController.js';
+import {createFlower, getAllFlowers, get8Feature, get3Sale, getAllSortCreatedAt, getFlowerById, updateFlower, deleteFlower} from '../controllers/flowerController.js';
 import {verifyToken, verifyAdmin} from '../middlewares/authMiddleware.js';
 
 router.post('/', verifyToken, verifyAdmin, createFlower);
 router.get('/', getAllFlowers);
+router.get('/featured-products', get8Feature);
+router.get('/sale-products', get3Sale);
+router.get('/product-list', getAllSortCreatedAt);
 router.get('/:id', getFlowerById);
 router.put('/:id', verifyToken, verifyAdmin, updateFlower);
 router.delete('/:id', verifyToken, verifyAdmin, deleteFlower);
