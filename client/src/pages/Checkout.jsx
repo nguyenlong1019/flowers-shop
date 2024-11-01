b529e12984d09f1f0fb0b7ebc48ae3f416b3eeca
nguyenlong1019 <nguyenvanlong17052002@gmail.com>
2024-11-01 13:49:45 +0700

    Fix UI

--- client/src/pages/Checkout.jsx
+++ client/src/pages/Checkout.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Checkout = () => {
-  
+  const [cart, setCart] = useState(() => {
+    const savedCart = localStorage.getItem('cart');
+    return savedCart ? JSON.parse(savedCart) : [];
+  });
+
+  const calculateTotal = () => {
+    return cart.reduce((total, item) => total + item.price * item.quantity, 0);
+  };
 
   return (
     <div className='checkout-page'>
@@ -38,13 +45,33 @@ const Checkout = () => {
           </div>
         </div>
 
-        <div className="checkout-order">
+        <div className="checkout-order" style={{backgroundColor: '#f8f8f8', padding: '24px'}}>
           <h3 className="checkout-title">
             Your Order
           </h3>
-
+          <div style={{padding: '24px'}}>
+            <div className="row" style={{paddingTop: '16px', paddingBottom: '16px', borderTop: '1px solid #ddd'}}>
+              <div className="col col-6" style={{textAlign: 'center'}}>PRODUCT</div>
+              <div className="col col-6" style={{textAlign: 'center'}}>TOTAL</div>
+            </div>
+            {cart.map(item => (
+              <>
+                <div className="row" style={{borderTop: '1px solid #ddd', padding: '12px 0'}}>
+                  <div className="col col-6">{item.name} x {item.quantity}</div>
+                  <div className="col col-6" style={{textAlign: 'center'}}>{(item.price * item.quantity).toLocaleString()} đ</div>
+                </div>
+              </>
+            ))}
+          </div>
           
-
+          <div className="form-group">
+            <label htmlFor="">Hình thức thanh toán</label>
+            <select style={{width: 'max-content'}} name="payment-type" id="payment-type">
+              <option style={{width: 'max-content'}} value="cod">Thanh toán khi nhận hàng</option>
+              <option style={{width: 'max-content'}} value="internet_banking">VNPay</option>
+            </select>
+          </div>
+          <button className='btn-checkout' type='submit' onClick={() => console.log("Clicked!")}>Process Checkout</button>
         </div>
       </div>
 
---
+++ server/routes/payment.js
@@ -0,0 +1,79 @@
+import express from "express";
+import moment from "moment";
+import crypto from "crypto";
+import querystring from "qs";
+const router = express.Router()
+
+router.post('/create_payment_url', function(req, res, next) {
+    process.env.TZ = 'Asia/Ho_Chi_Minh';
+
+    let date = new Date();
+    let createDate = moment(date).format('YYYYMMDDHHmmss');
+
+    let ipAddr = req.headers['x-forwarded-for'] || 
+        req.connection.remoteAddress || 
+        req.socket.remoteAddress || 
+        req.connection.socket.remoteAddress;
+
+    let tmnCode = 'DEND955G'
+    let secretKey = 'KYNYU155OS08SSGNQXRQ0R0HQ1MG2C8O'
+    let vnpUrl = 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html'
+    let returnUrl = 'http://localhost:8888/order/vnpay_return'
+    let orderId = moment(date).format('DDHHmmss');
+
+    // lay tu client 
+    let amount = req.body.amount;
+    let bankCode = req.body.bankCode; 
+
+    let locale = req.body.language;
+    if (locale === null || locale === '') {
+        locale = 'vn';
+    }  
+
+    let currCode = 'VND';
+    let vnp_Params = {};
+
+    vnp_Params['vnp_Version'] = '2.1.0';
+    vnp_Params['vnp_Command'] = 'pay';
+    vnp_Params['vnp_TmnCode'] = tmnCode;
+    vnp_Params['vnp_Locale'] = locale;
+    vnp_Params['vnp_CurrCode'] = currCode;
+    vnp_Params['vnp_TxnRef'] = orderId;
+    vnp_Params['vnp_OrderInfo'] = 'Thanh toan cho ma GD:' + orderId;
+    vnp_Params['vnp_OrderType'] = 'other';
+    vnp_Params['vnp_Amount'] = amount * 100;
+    vnp_Params['vnp_ReturnUrl'] = returnUrl;
+    vnp_Params['vnp_IpAddr'] = ipAddr;
+    vnp_Params['vnp_CreateDate'] = createDate;
+    if(bankCode !== null && bankCode !== ''){
+        vnp_Params['vnp_BankCode'] = bankCode;
+    }
+
+    vnp_Params = sortObject(vnp_Params);
+
+    let signData = querystring.stringify(vnp_Params, { encode: false });    
+    let hmac = crypto.createHmac("sha512", secretKey);
+    let signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex"); 
+    vnp_Params['vnp_SecureHash'] = signed;
+    vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
+
+    res.redirect(vnpUrl)
+});
+
+function sortObject(obj) {
+	let sorted = {};
+	let str = [];
+	let key;
+	for (key in obj){
+		if (obj.hasOwnProperty(key)) {
+		str.push(encodeURIComponent(key));
+		}
+	}
+	str.sort();
+    for (key = 0; key < str.length; key++) {
+        sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
+    }
+    return sorted;
+}
+
+export default router;
